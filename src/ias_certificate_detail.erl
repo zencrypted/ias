adb8afb705f6110724022737bbd53a41ef0f5dcb
cryoflamer <cryoflamer22@gmail.com>
2026-06-21 05:30:01 +0300

    Manual CA certificate classification was corrected

--- src/ias_certificate_detail.erl
+++ src/ias_certificate_detail.erl
@@ -2,13 +2,20 @@
 -export([metadata/1, security_policy/1, certificate_class/1, certificate_class_note/1]).
 
 certificate_class(#{kind := certificate} = Certificate) ->
-    Source = maps:get(source, Certificate, undefined),
     Id = maps:get(id, Certificate, <<>>),
-    case {Source, Id} of
-        {certificate_issue_demo, _} -> <<"Issued Identity Certificate">>;
-        {cmp_demo_enrollment, _} -> <<"Enrollment Certificate">>;
-        {ovpn_demo_import, _} -> <<"Imported OVPN Certificate">>;
-        _ -> certificate_class_from_id(Id)
+    case certificate_class_from_role(Certificate) of
+        unknown ->
+            case certificate_class_from_material(Certificate) of
+                unknown ->
+                    case certificate_class_from_source(Certificate) of
+                        unknown -> certificate_class_from_id(Id);
+                        Class -> Class
+                    end;
+                Class ->
+                    Class
+            end;
+        Class ->
+            Class
     end;
 certificate_class(_Certificate) ->
     <<"Unknown Certificate">>.
@@ -21,11 +28,34 @@ certificate_class_note(#{kind := certificate} = Certificate) ->
             <<"CA/CMP enrollment artifact; issue it to a user/security profile before role authorization applies">>;
         <<"Imported OVPN Certificate">> ->
             <<"Imported OpenVPN artifact used for migration, onboarding, and endpoint discovery">>;
+        <<"CA Certificate">> ->
+            <<"Operator-registered CA trust anchor used by VPN services. Public certificate material is stored separately from Demo State metadata.">>;
         _ ->
             <<"Certificate class could not be resolved from source metadata">>
     end;
 certificate_class_note(_Certificate) ->
     <<"Certificate class could not be resolved from source metadata">>.
+
+certificate_class_from_role(#{certificate_role := ca_certificate}) ->
+    <<"CA Certificate">>;
+certificate_class_from_role(_Certificate) ->
+    unknown.
+
+certificate_class_from_material(#{material_type := ca_certificate}) ->
+    <<"CA Certificate">>;
+certificate_class_from_material(_Certificate) ->
+    unknown.
+
+certificate_class_from_source(#{source := certificate_issue_demo}) ->
+    <<"Issued Identity Certificate">>;
+certificate_class_from_source(#{source := cmp_demo_enrollment}) ->
+    <<"Enrollment Certificate">>;
+certificate_class_from_source(#{source := ovpn_demo_import}) ->
+    <<"Imported OVPN Certificate">>;
+certificate_class_from_source(#{source := ca_certificate}) ->
+    <<"CA Certificate">>;
+certificate_class_from_source(_Certificate) ->
+    unknown.
 
 certificate_class_from_id(Id) when is_binary(Id) ->
     case has_prefix(Id, <<"issued_certificate_">>) of
@@ -36,7 +66,11 @@ certificate_class_from_id(Id) when is_binary(Id) ->
                 false ->
                     case has_prefix(Id, <<"ovpn_import_">>) of
                         true -> <<"Imported OVPN Certificate">>;
-                        false -> <<"Unknown Certificate">>
+                        false ->
+                            case has_prefix(Id, <<"manual_ca_certificate_">>) of
+                                true -> <<"CA Certificate">>;
+                                false -> <<"Unknown Certificate">>
+                            end
                     end
             end
     end;
--- test/ias_certificate_detail_tests.erl
+++ test/ias_certificate_detail_tests.erl
@@ -31,3 +31,69 @@ certificate_class_note_explains_enrollment_context_test() ->
                                                            source => cmp_demo_enrollment,
                                                            id => <<"cmp_enrollment_1_certificate">>}),
     ?assertMatch({_, _}, binary:match(Note, <<"issue it to a user/security profile">>)).
+
+manual_ca_object_is_ca_certificate_test() ->
+    Certificate = #{kind => certificate,
+                    source => ca_certificate,
+                    material_type => ca_certificate,
+                    certificate_role => ca_certificate,
+                    id => <<"manual_ca_certificate_1">>},
+
+    ?assertEqual(<<"CA Certificate">>,
+                 ias_certificate_detail:certificate_class(Certificate)).
+
+certificate_role_has_highest_priority_test() ->
+    Certificate = #{kind => certificate,
+                    source => certificate_issue_demo,
+                    material_type => client_certificate,
+                    certificate_role => ca_certificate,
+                    id => <<"issued_certificate_alice_1">>},
+
+    ?assertEqual(<<"CA Certificate">>,
+                 ias_certificate_detail:certificate_class(Certificate)).
+
+material_type_classifies_ca_certificate_test() ->
+    Certificate = #{kind => certificate,
+                    source => certificate_issue_demo,
+                    material_type => ca_certificate,
+                    id => <<"issued_certificate_alice_1">>},
+
+    ?assertEqual(<<"CA Certificate">>,
+                 ias_certificate_detail:certificate_class(Certificate)).
+
+source_classifies_ca_certificate_test() ->
+    Certificate = #{kind => certificate,
+                    source => ca_certificate,
+                    id => <<"some_id">>},
+
+    ?assertEqual(<<"CA Certificate">>,
+                 ias_certificate_detail:certificate_class(Certificate)).
+
+manual_ca_id_prefix_falls_back_to_ca_certificate_test() ->
+    Certificate = #{kind => certificate,
+                    id => <<"manual_ca_certificate_1782001_1">>},
+
+    ?assertEqual(<<"CA Certificate">>,
+                 ias_certificate_detail:certificate_class(Certificate)).
+
+ca_certificate_note_explains_material_boundary_test() ->
+    Note = ias_certificate_detail:certificate_class_note(#{kind => certificate,
+                                                           source => ca_certificate,
+                                                           id => <<"manual_ca_certificate_1">>}),
+
+    ?assertMatch({_, _}, binary:match(Note, <<"Operator-registered CA trust anchor">>)),
+    ?assertMatch({_, _}, binary:match(Note, <<"stored separately from Demo State metadata">>)).
+
+existing_certificate_classes_are_unchanged_test() ->
+    ?assertEqual(<<"Issued Identity Certificate">>,
+                 ias_certificate_detail:certificate_class(#{kind => certificate,
+                                                            source => certificate_issue_demo,
+                                                            id => <<"issued_certificate_alice_1">>})),
+    ?assertEqual(<<"Enrollment Certificate">>,
+                 ias_certificate_detail:certificate_class(#{kind => certificate,
+                                                            source => cmp_demo_enrollment,
+                                                            id => <<"cmp_enrollment_1_certificate">>})),
+    ?assertEqual(<<"Imported OVPN Certificate">>,
+                 ias_certificate_detail:certificate_class(#{kind => certificate,
+                                                            source => ovpn_demo_import,
+                                                            id => <<"ovpn_import_1_certificate">>})).
